07fcbb643eda8b93d09bb653ba62a0e8c546d89b
trentonandreoli <trentonandreoli@gmail.com>
2020-05-19 13:44:59 -0400

    Final Project WIP

---
+++ Final Project/custom.js
@@ -0,0 +1,9 @@
+$('#hide').click(function(){
+  console.log("Hide button clicked");
+  $('#IUMAP').hide();
+})
+
+$('#show').click(function(){
+  console.log("Show button clicked");
+  $('#IUMAP').show();
+})
