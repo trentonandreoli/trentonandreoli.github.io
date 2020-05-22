22a25edeb2a9698956fc47f261063fbbbf8b7118
trentonandreoli <trentonandreoli@gmail.com>
2020-05-21 21:58:24 -0400

    Final Not Optimized

--- Final Project/custom.js
+++ Final Project/custom.js
@@ -1,9 +1,19 @@
-$('#hide').click(function(){
+$('#hide1').click(function(){
   console.log("Hide button clicked");
   $('#IUMAP').hide();
 })
 
-$('#show').click(function(){
+$('#show1').click(function(){
   console.log("Show button clicked");
   $('#IUMAP').show();
+})
+
+$('#hide2').click(function(){
+  console.log("Hide button clicked");
+  $('#OAHQ').hide();
+})
+
+$('#show2').click(function(){
+  console.log("Show button clicked");
+  $('#OAHQ').show();
 })
