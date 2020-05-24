1fd352f297bc148021e26aa3a9b68e7336a6373a
trentonandreoli <trentonandreoli@gmail.com>
2020-05-24 18:03:45 -0400

    Final Project Recolored

---
+++ Final Project Edited/custom.js
@@ -0,0 +1,19 @@
+$('#hide1').click(function(){
+  console.log("Hide button clicked");
+  $('#IUMAP').hide();
+})
+
+$('#show1').click(function(){
+  console.log("Show button clicked");
+  $('#IUMAP').show();
+})
+
+$('#hide2').click(function(){
+  console.log("Hide button clicked");
+  $('#OAHQ').hide();
+})
+
+$('#show2').click(function(){
+  console.log("Show button clicked");
+  $('#OAHQ').show();
+})
